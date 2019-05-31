123e40688be2a901e10d18750c050b8bb6a10e20
Robert Ochoa <ochoa.robert1@gmail.com>
2019-05-31 16:44:58 -0400

    Inicializacion de Proyecto

---
+++ custom-woocommerce-invoices.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Plugin Name: Custom Woocommerce Invoices
+Plugin URI: http://www.robertochoa.com.ve/
+Author: Robert Ochoa
+Author URI: http://www.robertochoa.com.ve/
+Description: Plugin para enviar y controlar documentos como invoices y facturas para cada pedido en Woocommerce.
+Version: 1.0
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: cwoo_invoices
+Domain Path: /lang
+*/
+
+if ( !defined( 'ABSPATH' ) ) {
+    die( 'Direct access is forbidden.' );
+}
+/* THEME VARIABLES */
+$plugin_suffix = 'cwoo_invoices';
